c89cf68f765a2b6f1832dcf8ba7a0dc5877f506d
m1919810 <matl040527@qq.com>
2025-01-25 00:15:08 +0800

    feat:stupid AContainer

--- src/main/java/me/matl114/matlib/Implements/Slimefun/MachineRecipe/CraftingOperationAdaptor.java
+++ src/main/java/me/matl114/matlib/Implements/Slimefun/MachineRecipe/CraftingOperationAdaptor.java
@@ -3,6 +3,7 @@ package me.matl114.matlib.Implements.Slimefun.MachineRecipe;
 import io.github.thebusybiscuit.slimefun4.core.machines.MachineOperation;
 import io.github.thebusybiscuit.slimefun4.implementation.operations.CraftingOperation;
 import lombok.Getter;
+import lombok.Setter;
 import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.abstractItems.MachineRecipe;
 import org.bukkit.inventory.ItemStack;
 
@@ -17,5 +18,16 @@ public class CraftingOperationAdaptor<T extends MachineOperation>  extends Craft
     public CraftingOperationAdaptor(T operation) {
         super(new ItemStack[0],new ItemStack[0],0);
         handle = operation;
+    }
+    //for slimehud display use
+    @Setter
+    private int currentTick = 0 ;
+    public int getProgress() {
+        return this.currentTick;
+    }
+    @Setter
+    private int totalTick = 0 ;
+    public int getTotalTicks() {
+        return this.totalTick;
     }
 }
