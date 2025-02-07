c1bab6a49a82cc7430ada3a6aedc672f2939d729
m1919810 <matl040527@qq.com>
2025-02-07 15:28:58 +0800

    feat: more shit

---
+++ src/main/java/me/matl114/matlib/Utils/Algorithm/LazyInitReference.java
@@ -0,0 +1,28 @@
+package me.matl114.matlib.Utils.Algorithm;
+
+@SuppressWarnings("all")
+public class LazyInitReference<T extends Object> implements Cloneable {
+    public T value;
+    public boolean init;
+    private static LazyInitReference INSTANCE = new LazyInitReference();
+    private LazyInitReference() {}
+    public static <W extends Object> LazyInitReference<W> ofEmpty(){
+        return INSTANCE.clone();
+    }
+    public static <W extends Object> LazyInitReference<W> ofValue(W value){
+        LazyInitReference<W> result = INSTANCE.clone();
+        result.value = value;
+        result.init = true;
+        return result;
+    }
+    @Override
+    public LazyInitReference clone() {
+        try {
+            LazyInitReference clone = (LazyInitReference) super.clone();
+            // TODO: copy mutable state here, so the clone can't change the internals of the original
+            return clone;
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
+}
---
+++ src/main/java/me/matl114/matlib/Utils/Algorithm/LazyInitValue.java
@@ -0,0 +1,55 @@
+package me.matl114.matlib.Utils.Algorithm;
+
+import lombok.Value;
+
+import java.util.function.Supplier;
+
+@SuppressWarnings("all")
+public class LazyInitValue <T extends Object> implements Cloneable{
+    private T value;
+    private boolean initialized;
+    private Supplier<T> supplier;
+    private static LazyInitValue INSTANCE = new LazyInitValue();
+    private static Supplier<?> SUPPLIER = ()->null;
+    private LazyInitValue() {
+        value = null;
+        initialized = false;
+        supplier = (Supplier<T>) SUPPLIER;
+    }
+    public static <W extends Object> LazyInitValue<W> ofDirect(W valuy){
+        LazyInitValue<W> value = INSTANCE.clone();
+        value.initialized = true;
+        value.value = valuy;
+        return value;
+    }
+    public static <W extends Object> LazyInitValue<W> ofLazy(Supplier<W> valueGetter){
+        LazyInitValue<W> value = INSTANCE.clone();
+        value.supplier = valueGetter;
+        return value;
+    }
+    public LazyInitReference<T> toReference(){
+        return initialized?LazyInitReference.ofValue(value):LazyInitReference.ofEmpty();
+    }
+    public T get(){
+        if(!initialized){
+            value = supplier.get();
+        }
+        return value;
+    }
+    public void set(T value){
+        initialized = true;
+        this.value = value;
+    }
+
+    @Override
+    public LazyInitValue clone() {
+        try {
+            LazyInitValue clone = (LazyInitValue) super.clone();
+            // TODO: copy mutable state here, so the clone can't change the internals of the original
+            return clone;
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
+
+}
