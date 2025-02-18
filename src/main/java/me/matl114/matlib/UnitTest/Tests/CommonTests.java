1cc573fa6693b9db86085e15fc76eabdf01ddd3f
m1919810 <matl040527@qq.com>
2025-02-19 03:24:55 +0800

    feat: remove useless tests

--- src/main/java/me/matl114/matlib/UnitTest/Tests/CommonTests.java
+++ src/main/java/me/matl114/matlib/UnitTest/Tests/CommonTests.java
@@ -75,92 +75,59 @@ public class CommonTests implements TestCase {
         Debug.logger(locationLockFactory.getClass().getSimpleName());
         Debug.logger(locationLockFactory.checkThreadStatus(new Location(Bukkit.getWorlds().get(0),0,0,0)));
     }
-    public static Map<String,Integer> ObjectInvocationIndex = new HashMap<>(){{
-        put("getClass",-1);
-        put("hashCode",-2);
-        put("equals",-3);
-        put("clone",-4);
-        put("toString",-5);
-        put("notify",-6);
-        put("notifyAll",-7);
-        put("wait",-8);
-        put("wait0",-9);
-        put("finalize",-10);
-    }};
-    public static Object invokeBase(Object target,int index,Object[] args){
-        switch (index){
-            case -1:return target.getClass();
-            case -2:return target.hashCode();
-            case -3:return target.equals(args[0]);
-            case -4:throw new IllegalStateException("clone method not supported");
-            case -5:return target.toString();
-            case -6:target.notify();return null;
-            case -7:target.notifyAll();return null;
-            case -8:try{ switch (args.length){
-                case 0:target.wait();return null;
-                case 1:target.wait((Long) args[0]);return null;
-                case 2:target.wait();return null;
-                default:throw new IllegalArgumentException("Wrong argument count for wait!");
-            }}catch (InterruptedException e){
-            }
-            return null;
-            case -9:throw new IllegalArgumentException("wait0 method not supported");
-            case -10:throw new IllegalArgumentException("finalize method not supported");
-        }
-        return null;
-    }
+
     //测试耗时
     //
-    public class InvocationTest implements InvocationHandler{
-        private final Object target;
-        private final MethodAccess methodAccess;
-        private Map<MethodSignature,Integer> invocations =new HashMap<>();
-
-        public InvocationTest(Object value,String interfaceName){
-            this.target = value;
-
-            Class<?> targetInterface = ReflectUtils.getAllInterfacesRecursively(value.getClass()).stream().filter(e->interfaceName.equals(e.getSimpleName())).findAny().orElse(null);
-            Debug.logger(targetInterface);
-            this.methodAccess = MethodAccess.get(targetInterface);
-            Debug.logger((Object[])this.methodAccess.getMethodNames());
-            Debug.logger(methodAccess);
-            Debug.logger(AnnotationUtils.getAdaptorInstance(targetInterface).isPresent());
-            Debug.logger(AnnotationUtils.getAdaptedMethods(targetInterface));
-            for (Method method : value.getClass().getMethods()){
-                MethodSignature methodSignature = MethodSignature.getSignature(method);
-                Debug.logger(method,method.getDeclaringClass().getClassLoader());
-                try{
-                    Debug.logger(methodSignature.getSignatureIndex(methodAccess));
-                    invocations.put(methodSignature,methodAccess.getIndex(method.getName(),method.getParameterTypes()));
-                }catch (IllegalArgumentException baseMethod){
-
-                    if(ObjectInvocationIndex.containsKey(methodSignature.methodName())){
-                        Debug.logger("Base Method",methodSignature);
-                        invocations.put(methodSignature,ObjectInvocationIndex.get(methodSignature.methodName()));
-                    }else {
-                        Debug.logger("NotFound Method",methodSignature);
-                    }
-
-                }
-            }
-        }
-        @Override
-        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            Debug.logger(method.getDeclaringClass(),method.getDeclaringClass().getClassLoader());
-            MethodSignature sig = MethodSignature.getSignature(method);
-            Debug.logger(sig);
-            Debug.logger(invocations);
-            Debug.logger(invocations.get(sig));
-            int index = invocations.get(sig);
-            if(index >= 0){
-                Debug.logger("methodAccess",target.getClass().getClassLoader(),index, Arrays.toString(args));
-                return methodAccess.invoke(target,index,args);
-            }else {
-                return method.invoke(target,args);
-            }
-
-        }
-    }
+//    public class InvocationTest implements InvocationHandler{
+//        private final Object target;
+//        private final MethodAccess methodAccess;
+//        private Map<MethodSignature,Integer> invocations =new HashMap<>();
+//
+//        public InvocationTest(Object value,String interfaceName){
+//            this.target = value;
+//
+//            Class<?> targetInterface = ReflectUtils.getAllInterfacesRecursively(value.getClass()).stream().filter(e->interfaceName.equals(e.getSimpleName())).findAny().orElse(null);
+//            Debug.logger(targetInterface);
+//            this.methodAccess = MethodAccess.get(targetInterface);
+//            Debug.logger((Object[])this.methodAccess.getMethodNames());
+//            Debug.logger(methodAccess);
+//            Debug.logger(AnnotationUtils.getAdaptorInstance(targetInterface).isPresent());
+//            Debug.logger(AnnotationUtils.getAdaptedMethods(targetInterface));
+//            for (Method method : value.getClass().getMethods()){
+//                MethodSignature methodSignature = MethodSignature.getSignature(method);
+//                Debug.logger(method,method.getDeclaringClass().getClassLoader());
+//                try{
+//                    Debug.logger(methodSignature.getSignatureIndex(methodAccess));
+//                    invocations.put(methodSignature,methodAccess.getIndex(method.getName(),method.getParameterTypes()));
+//                }catch (IllegalArgumentException baseMethod){
+//
+//                    if(ObjectInvocationIndex.containsKey(methodSignature.methodName())){
+//                        Debug.logger("Base Method",methodSignature);
+//                        invocations.put(methodSignature,ObjectInvocationIndex.get(methodSignature.methodName()));
+//                    }else {
+//                        Debug.logger("NotFound Method",methodSignature);
+//                    }
+//
+//                }
+//            }
+//        }
+//        @Override
+//        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+//            Debug.logger(method.getDeclaringClass(),method.getDeclaringClass().getClassLoader());
+//            MethodSignature sig = MethodSignature.getSignature(method);
+//            Debug.logger(sig);
+//            Debug.logger(invocations);
+//            Debug.logger(invocations.get(sig));
+//            int index = invocations.get(sig);
+//            if(index >= 0){
+//                Debug.logger("methodAccess",target.getClass().getClassLoader(),index, Arrays.toString(args));
+//                return methodAccess.invoke(target,index,args);
+//            }else {
+//                return method.invoke(target,args);
+//            }
+//
+//        }
+//    }
     //public class MethodSignature2Int
 
 }
