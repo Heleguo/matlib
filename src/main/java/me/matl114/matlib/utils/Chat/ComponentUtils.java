53ac78f3826c5d3f0bb2a8d9958e2a76bd6fc0c3
m1919810 <matl040527@qq.com>
2025-04-24 04:07:40 +0800

    feat: comp

--- src/main/java/me/matl114/matlib/utils/Chat/ComponentUtils.java
+++ src/main/java/me/matl114/matlib/utils/Chat/ComponentUtils.java
@@ -5,19 +5,27 @@ import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import javax.annotation.Nonnull;
+
 public class ComponentUtils {
     private static final ThreadLocal<ItemMeta> DEFAULT_META = ThreadLocal.withInitial(()->new ItemStack(Material.STONE).getItemMeta());
+    public static final Component EMPTY = Component.empty();
 
-
-    public static Component fromLegacyString(String value){
+    public static final String EMPTY_STRING = "";
+    public static Component fromLegacyString(@Nonnull String value){
+        if(value.isEmpty()){
+            return EMPTY;
+        }
         ItemMeta meta = DEFAULT_META.get();
         meta.setDisplayName(value);
         return meta.displayName();
     }
 
-    public static String toLegacyString(Component component){
+
+    public static String toLegacyString(@Nonnull Component component){
         ItemMeta meta = DEFAULT_META.get();
         meta.displayName(component);
-        return meta.getDisplayName();
+        String name =  meta.getDisplayName();
+        return name == null ?"": name;
     }
 }
