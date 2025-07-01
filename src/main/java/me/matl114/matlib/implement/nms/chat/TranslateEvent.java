a553f61187d28fdb5259d0a84e054103c3ca39af
m1919810 <matl040527@qq.com>
2025-07-02 05:29:31 +0800

    feat: bugfix

--- src/main/java/me/matl114/matlib/implement/nms/chat/TranslateEvent.java
+++ src/main/java/me/matl114/matlib/implement/nms/chat/TranslateEvent.java
@@ -1,11 +1,13 @@
 package me.matl114.matlib.implement.nms.chat;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import me.matl114.matlib.algorithms.designs.event.Event;
 import me.matl114.matlib.implement.nms.network.ClientInformation;
 
 import java.util.Locale;
 
+@Getter
 public class TranslateEvent<T> extends Event {
     public TranslateEvent(ClientInformation information, T value, Locale locale){
         this.client = information;
